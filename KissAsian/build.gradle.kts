5ad52b5cd6c9e3af6e1705eead8ada4d1c199afc
chicanoandres702 <157326583+chicanoandres702@users.noreply.github.com>
2025-04-30 22:13:39 -0700

    Update build.gradle.kts

--- KissAsian/build.gradle.kts
+++ KissAsian/build.gradle.kts
@@ -1,5 +1,5 @@
 group = "com.demonstratorz" // Replace with your group ID
-version = 11 // Integer version number
+version = 12 // Integer version number
 
 cloudstream {
     description = "Kissasian Provider"
