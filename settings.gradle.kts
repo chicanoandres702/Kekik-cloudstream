8deea74e5cce38521014c1c1c73b41ec65ba6c8c
chicanoandres702 <157326583+chicanoandres702@users.noreply.github.com>
2025-05-02 00:34:41 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,16 +4,17 @@ rootProject.name = "CloudstreamPlugins"
 
 val disabled = listOf<String>("__Temel")
 
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
+//File(rootDir, ".").eachDir { dir ->
+//    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+//        include(dir.name)
+//    }
+//}
 
-fun File.eachDir(block: (File) -> Unit) {
-   listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
+//fun File.eachDir(block: (File) -> Unit) {
+//   listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+//}
 
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("Kissasian")
+include("Kissasian")
+include("Dramacool")
